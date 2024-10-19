1c195ab0154e8f449796776b6dda6426e565f070
Humza920 <humza.rehman.779@gmail.com>
2024-10-19 12:34:20 -0700

    DONE

--- PRACTICES/PRAC-2 (DATE AND TIME)/app.js
+++ PRACTICES/PRAC-2 (DATE AND TIME)/app.js
@@ -1,23 +1,40 @@
-let date = new Date() 
-console.log(date);
-console.log(date.getDate());
+// let date = new Date()
+// console.log(date);
+// console.log(date.getDate());
 
-var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-console.log(weekdays[date.getDay()]);
+// var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// console.log(weekdays[date.getDay()]);
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-console.log(months[date.getMonth()]);
+// var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+// console.log(months[date.getMonth()]);
 
-console.log(date.getFullYear());
-console.log(date.getHours() );
-console.log(date.getMinutes() );
-console.log(date.getSeconds())
-console.log(date.getMilliseconds())
-console.log(Math.floor(date.getTime()/(1000*60*60*24*365)));
+// console.log(date.getFullYear());
+// console.log(date.getHours() );
+// console.log(date.getMinutes() );
+// console.log(date.getSeconds())
+// console.log(date.getMilliseconds())
+// console.log(Math.floor(date.getTime()/(1000*60*60*24*365)));
 
-var doomsday = new Date("Jun 30, 2035")
-console.log(doomsday);
-var currms = date.getTime()
-console.log(doomsday.getTime());
+// var doomsday = new Date("Jun 30, 2035")
+// console.log(doomsday);
+// var currms = date.getTime()
+// console.log(currms);
 
+// var doom = doomsday.getTime()
+// console.log(doom);
 
+// var emerge =  doom - currms
+// console.log(Math.floor(emerge / (1000*60*60*24*365.25)));
+
+// var date = new Date();
+// date.setMonth(10)
+// date.setDate(10)
+// date.setFullYear(2020)
+// date.setHours(8)
+// date.setMinutes(20)
+// date.setSeconds(29);
+// console.log(date.toString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toTimeString());
