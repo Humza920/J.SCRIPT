fd763a2e67cc24c4aa19d8762d2a011e7f130e23
Humza920 <humza.rehman.779@gmail.com>
2024-10-24 12:38:50 -0700

    DON

--- ASSIGMNETS/TODO - APP/app.js
+++ ASSIGMNETS/TODO - APP/app.js
@@ -16,9 +16,33 @@ button.addEventListener("click", (e) => {
   list.innerHTML = "";
   for (let i = 0; i < arr.length; i++) {
     list.innerHTML += `
-    <li>${arr[i]}</li>    
+    <li>${arr[i]} <button id = "btn-1">Edit</button> <button id = "btn-2">Delete</button></li>    
     `;
+const btnOne = document.querySelector("#btn-1")
+const btnTwo = document.querySelector("#btn-2")
+
+btnOne.addEventListener("click" , (e) =>{
+  e.preventDefault()
+  arr.splice( arr[] , 1 , prompt("ENTER UPDATED TODO"))
+})
   }
 
   todo.value = "";
+
+// const btnOne = document.querySelector("#btn-1")
+// const btnTwo = document.querySelector("#btn-2")
+
+// btnOne.addEventListener("click" , (e) =>{
+//   e.preventDefault()
+//   arr.splice( e , 1 , prompt("ENTER UPDATED TODO"))
+// })
+
 });
+
+
+
+
+
+
+
+
