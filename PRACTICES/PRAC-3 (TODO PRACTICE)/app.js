c433a33f75d5451ee2aeb3f76ab7a7310bdacd88
Humza920 <humza.rehman.779@gmail.com>
2024-10-29 06:40:37 -0700

    DONE

--- PRACTICES/PRAC-3 (TODO PRACTICE)/app.js
+++ PRACTICES/PRAC-3 (TODO PRACTICE)/app.js
@@ -5,6 +5,14 @@
 // parent.removeChild(para)
 // }
 
+
+let idNum = 1
+const ul = document.getElementById("ul")
+const inpValue = document.getElementById("inp")
+
 function addTodo() {
-    
+    const create = document.createElement("li")
+    create.setAttribute("id" , idNum)
+    create.innerHTML = inpValue.value
+    idNum = idNum + 1
 }
