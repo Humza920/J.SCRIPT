e08b0dc3cc21e515ba3dd949cbf239b0888837ad
Humza920 <humza.rehman.779@gmail.com>
2024-10-28 22:28:03 -0700

    DONE

--- PRACTICES/PRAC - 4()/app.js
+++ PRACTICES/PRAC - 4()/app.js
@@ -24,20 +24,20 @@
 // div.appendChild(newElement);
 
         // classlist
-const div = document.querySelector("div");
+// const div = document.querySelector("div");
 
 // div.style.cssText = "color: red ; background-color : black;";
          
-const newElement = document.createElement("h1");
-newElement.innerHTML = `
-HUMZA REHMAN
-`;
+// const newElement = document.createElement("h1");
+// newElement.innerHTML = `
+// HUMZA REHMAN
+// `;
 // newElement.style.cssText = "color : white ; background-color : black;"
-newElement.className = "class-1  class-2  class-3"
-newElement.classList.remove("class-3") 
-newElement.classList.add("class-4") 
-newElement.classList.toggle("class-4" , true) 
-newElement.classList.toggle("class-4" , false) 
-// newElement.classList.replace("class-3" , "class-4")
-div.appendChild(newElement)
+// newElement.className = "class-1  class-2  class-3"
+// newElement.classList.remove("class-3") classlist.remove
+// newElement.classList.add("class-4") classlist.add
+// newElement.classList.toggle("class-4" , true) classlist.toggle
+// newElement.classList.toggle("class-4" , false) ''
+// newElement.classList.replace("class-3" , "class-4") classlist.replace
+// div.appendChild(newElement)
 
