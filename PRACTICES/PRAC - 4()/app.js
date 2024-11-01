1cea3972e1d05cd952b31abc21ad6c308f43f117
Humza920 <humza.rehman.779@gmail.com>
2024-10-31 21:37:18 -0700

    DONE

--- PRACTICES/PRAC - 4()/app.js
+++ PRACTICES/PRAC - 4()/app.js
@@ -2,4 +2,9 @@ const div = document.querySelector("div");
 
 // div.style.cssText = "color: red ; background-color : black;";
 
-const newElement = document.createElement("h1")
+const newElement = document.createElement("h1");
+newElement.innerHTML = `
+HUMZA REHMAN
+`;
+newElement.style.cssText = "color : white ; background-color : black;"
+div.appendChild(newElement);
