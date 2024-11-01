3a4e5e22249afe46783fe859c0d63ec78661653d
Humza920 <humza.rehman.779@gmail.com>
2024-10-31 22:08:43 -0700

    DONE

--- PRACTICES/PRAC - 4()/app.js
+++ PRACTICES/PRAC - 4()/app.js
@@ -1,10 +1,40 @@
-const div = document.querySelector("div");
+//          // cssText
+// const div = document.querySelector("div");
 
 // div.style.cssText = "color: red ; background-color : black;";
 
+// const newElement = document.createElement("h1");
+// newElement.innerHTML = `
+// HUMZA REHMAN
+// `;
+// newElement.style.cssText = "color : white ; background-color : black;"
+// div.appendChild(newElement);
+
+         // classname
+// const div = document.querySelector("div");
+
+// div.style.cssText = "color: red ; background-color : black;";
+         
+// const newElement = document.createElement("h1");
+// newElement.innerHTML = `
+// HUMZA REHMAN
+// `;
+// newElement.style.cssText = "color : white ; background-color : black;"
+// newElement.className = "class-1 , class-2 , class-3"
+// div.appendChild(newElement);
+
+        // classlist
+const div = document.querySelector("div");
+
+// div.style.cssText = "color: red ; background-color : black;";
+         
 const newElement = document.createElement("h1");
 newElement.innerHTML = `
 HUMZA REHMAN
 `;
-newElement.style.cssText = "color : white ; background-color : black;"
-div.appendChild(newElement);
+// newElement.style.cssText = "color : white ; background-color : black;"
+newElement.className = "class-1 , class-2 , class-3"
+// newElement.classList.remove("class-3") classlist.remove
+// newElement.classList.add("class-4") classlist.add
+newElement.classList.replace("class-3" , "class-4")
+div.appendChild(newElement)
