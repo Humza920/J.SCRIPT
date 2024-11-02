f34048f7c70c842c79cf763af4a86e37b3836035
Humza920 <humza.rehman.779@gmail.com>
2024-11-02 07:10:47 -0700

    DON

--- ASSIGMNETS/TODO FUNCTIONALITY 3/app.js
+++ ASSIGMNETS/TODO FUNCTIONALITY 3/app.js
@@ -1,28 +1,49 @@
+// Initialize a unique ID counter for each todo item
 let idNum = 1;
 const div = document.getElementById("main");
 const inpValue = document.getElementById("inp");
 
+// Function to add a new todo item
 function addTodo() {
   const createdUl = document.createElement("ul");
   createdUl.setAttribute("id", idNum);
-  createdUl.innerHTML = `
-    <li>${inpValue.value}</li> <button onclick = "edit(${idNum})">Edit</button> <button onclick = "remove(${idNum})">Delete</button>
+
+  if (inpValue.value === "") {
+    alert("Enter Todo First");
+  } else {
+    createdUl.innerHTML = `
+      <li>${inpValue.value}</li> 
+      <button onclick="edit(${idNum})">Edit</button> 
+      <button onclick="remove(${idNum})">Delete</button>
     `;
-  div.appendChild(createdUl);
-  idNum = idNum + 1;
+    div.appendChild(createdUl);
+    idNum = idNum + 1;
+  }
+
   inpValue.value = "";
 }
 
+// Function to edit an existing todo item
 function edit(id) {
   const todoItem = document.getElementById(id);
-  todoItem.innerHTML = ` <li>${prompt(
-    "ENTER"
-  )}</li> <button onclick = "edit(${id})">Edit</button> <button onclick = "remove(${id})">Delete</button>
+
+  let pom = prompt("Enter Updated Todo");
+
+  if (pom === "") {
+    alert("Please Enter Updated Todo");
+  } else {
+    todoItem.innerHTML = `
+      <li>${pom}</li> 
+      <button onclick="edit(${id})">Edit</button> 
+      <button onclick="remove(${id})">Delete</button>
     `;
+  }
 }
 
+// Function to remove a todo item
 function remove(id) {
   const todoItem = document.getElementById(id);
   const parent = todoItem.parentNode;
+
   parent.removeChild(todoItem);
 }
