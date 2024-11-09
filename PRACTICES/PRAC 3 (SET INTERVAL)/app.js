f8e6debfee64cb2036472dd99c554697467786ae
Humza920 <humza.rehman.779@gmail.com>
2024-11-09 17:44:11 +0500

    DONE

--- PRACTICES/PRAC 3 (SET INTERVAL)/app.js
+++ PRACTICES/PRAC 3 (SET INTERVAL)/app.js
@@ -1,32 +1,85 @@
-const h1 = document.querySelector("h1");
-const btnStart = document.querySelector("#start");
-const btnPause = document.querySelector("#pause");
-const btnReset = document.querySelector("#reset");
+// const h1 = document.querySelector("h1");
+// const btnStart = document.querySelector("#start");
+// const btnPause = document.querySelector("#pause");
+// const btnReset = document.querySelector("#reset");
 
-btnStart.addEventListener("click", (e) => {
-  e.preventDefault();
-  let hour = 0;
-  let min = 0;
-  let sec = 0;
-  let interval = null
+// let interval = null;
+// let hour = 0;
+// let min = 0;
+// let sec = 0;
 
-if (interval === null) {
-  interval = setInterval(() => {
-    sec++;
-    if (sec === 60) {
-      min++;
-      sec = 0;
-    }
-    if (min === 60) {
-      hour++;
-      min = 0;
-    }
+// btnStart.addEventListener("click", (e) => {
+//   e.preventDefault();
 
-    h1.innerHTML = `${hour} : ${min} : ${sec}`;
-  }, 1000);
+//   if (interval === null) {
+//     interval = setInterval(() => {
+//       sec++;
+//       if (sec === 60) {
+//         min++;
+//         sec = 0;
+//       }
+//       if (min === 60) {
+//         hour++;
+//         min = 0;
+//       }
+
+//       h1.innerHTML = `${hour} : ${min} : ${sec}`;
+//     }, 1000);
+//   }
+
+//   btnPause.addEventListener("click", (e) => {
+//     e.preventDefault();
+//       clearInterval(interval)
+//       interval = null
+//   });
+
+//   btnReset.addEventListener("click" , (e)=>{
+//     e.preventDefault()
+//     location.reload()
+    
+//   })
+
+// });
+
+
+
+
+
+// let init = 0
+// function round(){
+//   var box = document.querySelector('#box');
+
+
+//   var round = init += 90;
+//   box.style.rotate = `${round}deg`
+// box.style.backgroundColor = `#${hexColor}`
+  // box.style.transition = '0.4s ease-in-out all' 
+// }
+// function round() {
+//   const box = document.getElementById("box")
+//  let deg = init += 50
+//   box.style.rotate = `${deg}deg`
+// deg = deg + 60
+// }
+
+// setInterval(round, 100);
+
+
+
+const btn = document.querySelector("#Btn")
+let init = 0
+let interval = null
+
+function round() {
+if (interval) {
+  clearInterval(interval)
+  interval = null
+} else {
+ interval = setInterval( ()=>{
+  const box = document.getElementById("box")
+  let deg = init += 50
+  box.style.rotate = `${deg}deg`
+ } , 100);
 }
-else{
-  alert("Timer is already running");
 }
-   
-});
+
