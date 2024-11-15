0ddf5e5bca5b72e6787076428bf2e0ba878e2e3f
Humza920 <humza.rehman.779@gmail.com>
2024-11-15 09:02:15 +0500

    DONE

--- PRACTICES/PRAC  - 6/app.js
+++ PRACTICES/PRAC  - 6/app.js
@@ -1,19 +1,47 @@
-function fizzBuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    let output = i;
-    if (i % 3 != 0 && i % 5 != 0) {
-      output = i;
-      console.log(output);
-    }
-    if (i % 3 === 0) {
-      output += "Fizz";
-      console.log(output);
-    }
-    if (i % 5 === 0) {
-      output += "Buzz";
-      console.log(output);
+// FizzBuzz Variation
+
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let output = i;
+//     if (i % 3 != 0 && i % 5 != 0) {
+//       output = i;
+//     }
+//     if (i % 3 === 0) {
+//       output += "Fizz";
+//     }
+//     if (i % 5 === 0) {
+//       output += "Buzz";
+//     }
+//     console.log(output);
+//   }
+// }
+
+// fizzBuzz(+prompt("ENTER NUMBER"));
+
+const str = "javascript";
+
+function check(str) {
+  console.log(str);
+  let count = [];
+
+  // for (let i = 0; i < str.length; i++) {
+  //     console.log(str[i]);
+  //     console.log(count[i]);
+
+  //     // if (count[i]) {
+
+  //     // }
+  // }
+  for (const i of str) {
+    // console.log(i);
+    // console.log(str);
+    if (count[i]) {
+      count[i]++;
+    } else {
+    count[i] = 1;
     }
   }
+  return count;
 }
+console.log(check(str));
 
-fizzBuzz(+prompt("ENTER NUMBER"));
