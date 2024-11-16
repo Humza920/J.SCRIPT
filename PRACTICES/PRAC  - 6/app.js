7674d04a0cd788169e6d532d826ac30240a7628a
Humza920 <humza.rehman.779@gmail.com>
2024-11-16 13:10:11 +0500

    DONE

--- PRACTICES/PRAC  - 6/app.js
+++ PRACTICES/PRAC  - 6/app.js
@@ -18,23 +18,42 @@
 
 // fizzBuzz(+prompt("ENTER NUMBER"));
 
-const str = "javascript";
-let count = {};
+// const str = "javascript";
+// let count = {};
 
-function check(str) {
-  console.log(str);
+// function check(str) {
+//   console.log(str);
 
-  for (let i = 0; i < str.length; i++) {
-      console.log(str[i]);
-      if (count[str[i]]) {
-        count[str[i]]++
-      } else {
-        count[str[i]] = 1
-      }
-  }
+//   for (let i = 0; i < str.length; i++) {
+//       console.log(str[i]);
+//       if (count[str[i]]) {
+//         count[str[i]]++
+//       } else {
+//         count[str[i]] = 1
+//       }
+//   }
 
-  return count;
-}
-console.log(check(str));
+//   return count;
+// }
+// console.log(check(str));
 
-console.log(count[0]);
+// let str = "swiss";
+// let count = 0;
+// function check(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     count = 0;
+//     for (let j = 0; j < str.length; j++) {
+//       if (str[i] === str[j]) {
+//         console.log(++count);
+//       }
+//     }
+//     if (count == 1) {
+//       let ans = str[i];
+//       console.log(ans);
+
+//       break;
+//     }
+//   }
+// }
+
+// check(str);
