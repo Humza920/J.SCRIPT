665b1d98fbd0f67de44df2c7317df0bb50587ba0
Humza920 <humza.rehman.779@gmail.com>
2024-11-29 10:50:05 +0500

    DONE

---
+++ ASSIGMNETS/CALCULATOR/app.js
@@ -0,0 +1,21 @@
+let string = "";
+let buttons = document.querySelectorAll("button");
+
+console.log(buttons);
+
+let inToArray = Array.from(buttons)
+console.log(inToArray);
+
+
+inToArray.forEach(button=>{
+    button.addEventListener("click",function(){
+        string += button.textContent;
+        console.log(string);
+        let input =document.querySelector(".display")
+        console.log(input);
+        input.value = string;
+        
+        })
+        
+    
+})
