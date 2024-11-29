ad091afb48b58922c98ab6bba9dd9a49034f4c42
Humza920 <humza.rehman.779@gmail.com>
2024-11-29 16:45:28 +0500

    DONE

--- ASSIGMNETS/CALCULATOR/app.js
+++ ASSIGMNETS/CALCULATOR/app.js
@@ -3,24 +3,27 @@ let buttons = document.querySelectorAll("button");
 
 console.log(buttons);
 
-let inToArray = Array.from(buttons)
+let inToArray = Array.from(buttons);
 console.log(inToArray);
 
-
-inToArray.forEach(button=>{
-
-    button.addEventListener("click",function(){
-        if (button.innerHTML == "=") {
-            
-        } else {
-            
-        }
-        string += button.textContent;
-        console.log(string);
-        let input =document.querySelector(".display")
-        console.log(input);
-        input.value = string;
-        })
-        
-    
-})
+inToArray.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (button.innerHTML === "=") {
+      string = eval(string);
+      let input = document.querySelector(".display");
+      input.value = string;
+      console.log(input.innerHTML);
+    } else if (button.innerHTML == "AC") {
+      let input = document.querySelector(".display");
+      string = ""
+      input.value = "";
+      console.log(input.innerHTML);
+    } else {
+      string += button.textContent;
+      console.log(string);
+      let input = document.querySelector(".display");
+      input.value = string;
+      console.log(input.innerHTML);
+    }
+  });
+});
