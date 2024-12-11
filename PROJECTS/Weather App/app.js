6961b861398d2875c88082a32ab1bb4b2c7e699f
Humza920 <humza.rehman.779@gmail.com>
2024-12-11 17:44:25 +0500

    DONE

--- PROJECTS/Weather App/app.js
+++ PROJECTS/Weather App/app.js
@@ -10,53 +10,61 @@ const btn = document.querySelector("button")
 const input = document.querySelector("input")
 
 async function defalt() {
-    let apiKey = "3c7a07e2b676687d3241b2e719cd4943"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=karachi`
-    const data = await fetch(apiUrl + `&appid=${apiKey}`)
-    const weatherData = await data.json()
-    console.log(weatherData);
+    try {
+        let apiKey = "3c7a07e2b676687d3241b2e719cd4943"
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=karachi`
+        const data = await fetch(apiUrl + `&appid=${apiKey}`)
+        const weatherData = await data.json()
+        console.log(weatherData);
 
-    img.setAttribute("class", "weather-icon")
-    weatherDiv.appendChild(img)
+        img.setAttribute("class", "weather-icon")
+        weatherDiv.appendChild(img)
 
-    if (weatherData.weather[0].main == "Clouds") {
-        img.src = "Assets/cloud.png"
-    }
-    else if (weatherData.weather[0].main == 'Clear') {
-        img.src = "Assets/clear.png"
-    }
-    else if (weatherData.weather[0].main == "Rain") {
-        img.src = "Assets/rain.png"
-    }
-    else if (weatherData.weather[0].main == "Drizzle") {
-        img.src = "Assets/drizzle.png"
-    }
-    else if (weatherData.weather[0].main == "Mist") {
-        img.src = "Assets/mist.png"
-    }
-    else if (weatherData.weather[0].main == "Wind") {
-        img.src = "Assets/wind.png"
-    }
-    else if (weatherData.weather[0].main == "Snow") {
-        img.src = "Assets/snow.png"
-    }
-    else {
-        img.src = "Assets/clouds.png"
-    }
+        if (weatherData.weather[0].main == "Clouds") {
+            img.src = "Assets/clouds.png"
+        }
+        else if (weatherData.weather[0].main == 'Clear') {
+            img.src = "Assets/clear.png"
+        }
+        else if (weatherData.weather[0].main == "Rain") {
+            img.src = "Assets/rain.png"
+        }
+        else if (weatherData.weather[0].main == "Drizzle") {
+            img.src = "Assets/drizzle.png"
+        }
+        else if (weatherData.weather[0].main == "Mist") {
+            img.src = "Assets/mist.png"
+        }
+        else if (weatherData.weather[0].main == "Wind") {
+            img.src = "Assets/wind.png"
+        }
+        else if (weatherData.weather[0].main == "Snow") {
+            img.src = "Assets/snow.png"
+        }
+        else {
+            img.src = "Assets/clouds.png"
+        }
 
 
-    h1.setAttribute("class", "temp")
-    weatherDiv.appendChild(h1)
-    h1.innerHTML = `${Math.round(weatherData.main.temp)}°C`
+        h1.setAttribute("class", "temp")
+        weatherDiv.appendChild(h1)
+        h1.innerHTML = `${Math.round(weatherData.main.temp)}°C`
 
-    h2.setAttribute("class", "city")
-    h2.textContent = `Karachi`
-    weatherDiv.appendChild(h2)
+        h2.setAttribute("class", "city")
+        h2.textContent = `Karachi`
+        weatherDiv.appendChild(h2)
 
-    p1.innerHTML = `${weatherData.main.humidity}%`
-    p2.innerHTML = `Humidity`
-    p3.innerHTML = `${weatherData.wind.speed} km/h`
-    p4.innerHTML = `Wind Speed`
+        p1.innerHTML = `${weatherData.main.humidity}%`
+        p2.innerHTML = `Humidity`
+        p3.innerHTML = `${weatherData.wind.speed} km/h`
+        p4.innerHTML = `Wind Speed`
+
+
+    } catch (error) {
+        console.log(`Error ${error.message}`);
+        alert(error)
+    }
+
 
 }
 defalt()
@@ -65,43 +73,50 @@ let apiKey = "3c7a07e2b676687d3241b2e719cd4943"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`
 
 async function check(city) {
-    const data = await fetch(apiUrl + city + `&appid=${apiKey}`)
-    const weatherData = await data.json()
-    console.log(weatherData);
-
-    if (weatherData.weather[0].main == "Clouds") {
-        img.src = "Assets/clouds.png"
+    try {
+        const data = await fetch(apiUrl + city + `&appid=${apiKey}`)
+        const weatherData = await data.json()
+        console.log(weatherData);
+    
+        if (weatherData.weather[0].main == "Clouds") {
+            img.src = "Assets/clouds.png"
+        }
+        else if (weatherData.weather[0].main == 'Clear') {
+            img.src = "Assets/clear.png"
+        }
+        else if (weatherData.weather[0].main == "Rain") {
+            img.src = "Assets/rain.png"
+        }
+        else if (weatherData.weather[0].main == "Drizzle") {
+            img.src = "Assets/drizzle.png"
+        }
+        else if (weatherData.weather[0].main == "Mist") {
+            img.src = "Assets/mist.png"
+        }
+        else if (weatherData.weather[0].main == "Wind") {
+            img.src = "Assets/wind.png"
+        }
+        else if (weatherData.weather[0].main == "Snow") {
+            img.src = "Assets/snow.png"
+        }
+        else {
+            img.src = "Assets/clouds.png"
+        }
+    
+        h1.innerHTML = `${Math.round(weatherData.main.temp)}°C`
+        h2.innerHTML = weatherData.name
+        p1.innerHTML = `${weatherData.main.humidity}%`
+        p2.innerHTML = `Humidity`
+        p3.innerHTML = `${weatherData.wind.speed} km/h`
+        p4.innerHTML = `Wind Speed`
+    
+        input.value = ""
+        
+    } catch (error) {
+        console.log(error);
+        alert(error)
     }
-    else if (weatherData.weather[0].main == 'Clear') {
-        img.src = "Assets/clear.png"
-    }
-    else if (weatherData.weather[0].main == "Rain") {
-        img.src = "Assets/rain.png"
-    }
-    else if (weatherData.weather[0].main == "Drizzle") {
-        img.src = "Assets/drizzle.png"
-    }
-    else if (weatherData.weather[0].main == "Mist") {
-        img.src = "Assets/mist.png"
-    }
-    else if (weatherData.weather[0].main == "Wind") {
-        img.src = "Assets/wind.png"
-    }
-    else if (weatherData.weather[0].main == "Snow") {
-        img.src = "Assets/snow.png"
-    }
-    else {
-        img.src = "Assets/clouds.png"
-    }
-
-    h1.innerHTML = `${Math.round(weatherData.main.temp)}°C`
-    h2.innerHTML = weatherData.name
-    p1.innerHTML = `${weatherData.main.humidity}%`
-    p2.innerHTML = `Humidity`
-    p3.innerHTML = `${weatherData.wind.speed} km/h`
-    p4.innerHTML = `Wind Speed`
-
-    input.value = ""
+ 
 }
 
 btn.addEventListener("click", (e) => {
