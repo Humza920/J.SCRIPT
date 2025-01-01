3c66a5059a7a0085f107e4281b71980e45c41d00
Humza920 <humza.rehman.779@gmail.com>
2025-01-01 17:39:10 +0500

    DONE

--- PRACTICES/PROMISES/app.js
+++ PRACTICES/PROMISES/app.js
@@ -26,16 +26,33 @@
 // }
 
 
-const div = document.getElementById("buttons")
-const input = document.querySelector("input")
-const butn = document.querySelector("button")
-let count = 0
+// const div = document.getElementById("buttons")
+// const input = document.querySelector("input")
+// const butn = document.querySelector("button")
+// let count = 0
 
-butn.addEventListener("click" , (e)=>{
-    e.preventDefault()
-    for (let i = 0; i < input.value; i++) {  
-        div.innerHTML += `<button type="button">${++count}</button>`
+// butn.addEventListener("click" , (e)=>{
+//     e.preventDefault()
+//     for (let i = 0; i < input.value; i++) {  
+//         div.innerHTML += `<button type="button">${++count}</button>`
         
-        }    
+//         }    
         
-})
+// })
+
+
+async function dataFetch(){
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    console.log(data);
+    
+    const result = await data.json()
+    console.log(result)
+
+
+    // data.then((res)=> {
+    //     return res.json()
+    // }).then((data)=>{
+    //     console.log(data)
+    // })
+}
+dataFetch()
