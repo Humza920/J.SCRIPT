25eea9498844db15d7410c9c1b176d0a67bb15e8
Humza920 <humza.rehman.779@gmail.com>
2025-01-23 02:22:47 +0500

    DONE

---
+++ ASSIGMNETS/SIMPLE - ECOMMERCE/app.js
@@ -0,0 +1,121 @@
+const carousel = document.querySelector("#carouselExampleIndicators")
+async function check() {
+    const data = await fetch('https://fakestoreapi.com/products')
+    const show = await data.json()
+    console.log(show);
+
+    let img = show.map((item) => {
+        return item.image
+    })
+
+    console.log(img);
+
+
+
+    carousel.innerHTML += `
+    
+    <div class="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="${img[0]}" class="d-block w-100" alt="..." />
+              </div>
+              <div class="carousel-item">
+                <img src="${img[1]}" class="d-block w-100" alt="..." />
+              </div>
+              <div class="carousel-item">
+                <img src="${img[2]}" class="d-block w-100" alt="..." />
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+              
+            >
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+    
+    `
+}
+
+
+
+
+
+check()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
