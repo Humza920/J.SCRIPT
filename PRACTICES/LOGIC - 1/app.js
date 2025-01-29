441c0cdf7f02cdb42cb80fecf6a2428330c0cad1
Humza920 <humza.rehman.779@gmail.com>
2025-01-30 01:14:46 +0500

    DONE

---
+++ PRACTICES/LOGIC - 1/app.js
@@ -0,0 +1,72 @@
+// a. Sum of Two Numbers:
+// Sawal: Ek function likho jo 2 numbers ka sum return kare. Function ka naam sum rakho aur usme 2 parameters a aur b hon.
+
+// function sum(a , b) {
+//     return a + b
+// }
+
+// console.log(sum(2 , 3));
+
+// b. Check Even or Odd:
+// Sawal: Ek function likho jo check kare ke number even hai ya odd. Function ka naam checkEvenOdd rakho aur usme ek parameter num hon.
+
+// function checkEvenOdd(num) {
+//     if (num % 2 === 0) {
+//         console.log("EVEN");
+
+//     } else {
+//         console.log("ODD"); 
+//     }
+// }
+// checkEvenOdd(27)
+
+// a. Print Numbers from 1 to N:
+// Sawal: Ek function likho jo 1 se N tak numbers print kare. Function ka naam printNumbers rakho aur usme ek parameter n hon.
+// let num ;
+// function printNumbers(n) {
+//     for (let i = 1; i <= n; i++) {
+//          num = i
+//          console.log(num);
+//         }
+//         return num
+
+// }
+// console.log(printNumbers(28));
+
+// Print Even Numbers from 1 to N:
+// Sawal: Ek function likho jo 1 se N tak ke saare even numbers print kare. Function ka naam printEvenNumbers rakho aur usme ek parameter n hon.
+
+// let num = [1 ,2 ,3 ,4, 5, 6, 7 ,8 ,9 ,10]
+// function printEvenNumbers(n) {
+//     for (let i = 1; i <= n.length; i++) {        
+//         if (i % 2 === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// printEvenNumbers(num)
+
+// Find Maximum Number in an Array:
+// Sawal: Ek function likho jo ek array mein se maximum number find kare. Function ka naam findMax rakho aur usme ek parameter arr hon.
+
+// let numbers = [39, 45, 43, 32, 102, 79, 140, 120]
+
+// function findMax(arr) {
+// let max = arr[0]
+// // console.log(max);
+
+// for (let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+    
+//     if (max<arr[i]) {      
+//         max = arr[i]
+//         // console.log(max);
+        
+//     }
+// }
+// console.log(max);
+// return max
+// }
+
+// console.log(findMax(numbers));
